24efd2eb033fc167486cde73e16de9d83d6f0c89
Vlad Golubev <holybay1886@gmail.com>
2024-10-28 14:01:04 +0200

    Moved getting a user_login_form.jsp by another Servlet.

--- src/main/java/by/it_academy/jd2/golubev_107/messenger/controller/servlet/LoginServlet.java
+++ src/main/java/by/it_academy/jd2/golubev_107/messenger/controller/servlet/LoginServlet.java
@@ -22,11 +22,6 @@ public class LoginServlet extends HttpServlet {
     private final IUserService service = ServiceFactory.getInstance().getUserService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/jsp/user_login_form.jsp").forward(req, resp);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         try {
